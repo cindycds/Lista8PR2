19848c88f998e287602fc2bc7a0469311a39d0a0
cindycds <cindy.silva@aluno.ifsp.edu.br>
2023-05-25 10:53:54 -0300

    5 pronto

--- Lista8PR2/Program.cs
+++ Lista8PR2/Program.cs
@@ -13,24 +13,280 @@ namespace Lista8PR2
 
         public static void Exercicio1()
         {
+            float quant = 1, valor, totalval = 0, totalitens = 0;
+            do
+            {
+
+                quant++;
+                Console.WriteLine("Digite a quantidade do produto,quando quiser terminar digite 0:");
+                quant = float.Parse(Console.ReadLine());
+                if (quant == 0)
+                    break;
+
+                Console.WriteLine("Digite o  valor desses produto :");
+                valor = float.Parse(Console.ReadLine());
+
+                totalitens += quant;
+                totalval += quant * valor;
+
+            } while (true);
+            Console.WriteLine("a quandidade de produtos é:" + totalitens);
+            Console.WriteLine("o valor total da suacompra é:" + totalval);
 
 
         }
         public static void Exercicio2()
         {
+            string nome, nomemaio = "", nomemenor = "";
+            int numitens, parada = 2;
+            float preco, menor, maior;
+
+            maior = float.MaxValue;
+            menor = float.MinValue;
+
+            do
+            {
+                Console.WriteLine(" entre com o nome do produto");
+                nome = Console.ReadLine();
+
+                Console.WriteLine(" entre com o total de itens :");
+                numitens = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("entrecom o preço unitario");
+                preco = float.Parse(Console.ReadLine());
+
+                float total = numitens * preco;
+                if (total > maior)
+                {
+                    maior = total;
+                    nomemaio = nome;
+
+                }
+                if (total < menor)
+                {
+                    menor = total;
+                    nomemenor = nome;
+                }
+
+                Console.WriteLine(" digite 1 para sair e 2 para continuar: ");
+                parada = int.Parse(Console.ReadLine());
+                Console.WriteLine("maior quantia investida {0} no produto {1}", maior, nomemaio);
+                Console.WriteLine("maior quantia investida {0} no produto {1}", menor, nomemenor);
+
+            } while (parada != 1);
+
 
         }
         public static void Exercicio3()
         {
+            float pesos, tpesos = 0;
+            Console.WriteLine("Digite a quantidade de avaliaçoes que voce fará:");
+            int quantavali = int.Parse(Console.ReadLine());
 
+            int c = 1;
+            do
+            {
+                c++;
+                Console.WriteLine("Digite os pesos das notas {i}:");
+                pesos = float.Parse(Console.ReadLine());
+                tpesos += pesos;
+            } while (c <= quantavali);
+                Console.WriteLine("a soma dos pesos é: " + tpesos);
+            if (tpesos < 100)
+                Console.WriteLine("Pesos insuficientes");
+
+            else if (tpesos > 100)
+                Console.WriteLine("superior a 100% alcansada");
+
+
+            else
+            {
+                Console.WriteLine("porcentagem de 100% alcansada");
+            }
         }
+            
+        
         public static void Exercicio4()
         {
 
+
+            string sigla;
+            do
+            {
+
+                
+                Console.WriteLine("informe a sigla do estado desejado, ou SS para sair:");
+                sigla = Console.ReadLine();
+
+                switch (sigla)
+                {
+                    case "AC":
+                        Console.WriteLine("sigla referente ao Acre");
+                        break;
+
+                    case "AL":
+                        Console.WriteLine("sigla referente ao Alagoas");
+                        break;
+                    case "AP":
+                        Console.WriteLine("sigla referente ao Amapá");
+                        break;
+                    case "AM":
+                        Console.WriteLine("sigla referente ao Amazonas");
+                        break;
+                    case "BA":
+                        Console.WriteLine("sigla referente ao Bahia");
+                        break;
+                    case "CE":
+                        Console.WriteLine("sigla referente ao Ceará");
+                        break;
+                    case "DF":
+                        Console.WriteLine("sigla referente ao Distrito Federal");
+                        break;
+                    case "ES":
+                        Console.WriteLine("sigla referente ao Espirito Santo");
+                        break;
+                    case "GO":
+                        Console.WriteLine("sigla referente ao Goiás");
+                        break;
+                    case "MA":
+                        Console.WriteLine("sigla referente ao Maranhão");
+                        break;
+                    case "MT":
+                        Console.WriteLine("sigla referente ao Mato Grosso");
+                        break;
+                    case "MS":
+                        Console.WriteLine("sigla referente ao Mato Grosso do Sul");
+                        break;
+                    case "MG":
+                        Console.WriteLine("sigla referente ao Minas Gerais");
+                        break;
+                    case "PA":
+                        Console.WriteLine("sigla referente ao Pará");
+                        break;
+                    case "PB":
+                        Console.WriteLine("sigla referente ao Paraíba");
+                        break;
+                    case "PR":
+                        Console.WriteLine("sigla referente ao Paraná");
+                        break;
+                    case "PE":
+                        Console.WriteLine("sigla referente ao Pernambuco");
+                        break;
+                    case "PI":
+                        Console.WriteLine("sigla referente ao Piauí");
+                        break;
+                    case "Rj":
+                        Console.WriteLine("sigla referente ao Rio de Janeiro");
+                        break;
+                    case "RN":
+                        Console.WriteLine("sigla referente ao Rio Grande do Norte");
+                        break;
+                    case "RS":
+                        Console.WriteLine("sigla referente ao Rio Grande do Sul");
+                        break;
+                    case "RO":
+                        Console.WriteLine("sigla referente ao Rondonia");
+                        break;
+                    case "RR":
+                        Console.WriteLine("sigla referente ao Roraima");
+                        break;
+                    case "SC":
+                        Console.WriteLine("sigla referente ao Santa Catarina");
+                        break;
+                    case "SP":
+                        Console.WriteLine("sigla referente ao São Paulo");
+                        break;
+                    case "SE":
+                        Console.WriteLine("sigla referente ao Sergipe");
+                        break;
+                    case "TO":
+                        Console.WriteLine("sigla referente ao Tocantins");
+                        break;
+                    case "SS":
+                        Console.WriteLine("seu programa sera finalizado");
+                        break;
+                    default:
+                        Console.WriteLine("Sigla invalida");
+                        break;
+
+                }
+            }while (sigla!="SS");
         }
         public static void Exercicio5()
         {
+            int mes, dia;
+            do
+            {
 
+
+                Console.WriteLine("Digite o numero do mes em que deseja, ou 13 para encerrar:");
+                mes = int.Parse(Console.ReadLine());
+                switch (mes)
+                {
+                    case (1):
+                        Console.WriteLine("A estação é verão");
+                        break;
+                    case (2):
+                        Console.WriteLine("A estação é verão");
+                        break;
+                    case (3):
+                        Console.WriteLine("me informe o dia:");
+                        dia = int.Parse(Console.ReadLine());
+                        if (dia >= 20)
+                            Console.WriteLine("a estaçao é outono");
+                        else
+                            Console.WriteLine("A estação é verão");
+                        break;
+                    case (4):
+                        Console.WriteLine("A estação é outono");
+                        break;
+                    case (5):
+                        Console.WriteLine("A estação é outono");
+                        break;
+                    case (6):
+                        Console.WriteLine("me informe o dia:");
+                        dia = int.Parse(Console.ReadLine());
+                        if (dia >= 20)
+                            Console.WriteLine("a estaçao é inverno");
+                        else
+                            Console.WriteLine("A estação é outono");
+                        break;
+                    case (7):
+                        Console.WriteLine("a estaçao é inverno");
+                        break;
+                    case (8):
+                        Console.WriteLine("a estaçao é inverno");
+                        break;
+                    case (9):
+                        Console.WriteLine("me informe o dia:");
+                        dia = int.Parse(Console.ReadLine());
+                        if (dia >= 22)
+                            Console.WriteLine("a estaçao é primavera");
+                        else
+                            Console.WriteLine("A estação é inverno");
+                        break;
+                    case (10):
+                        Console.WriteLine("a estaçao é primavera");
+                        break;
+                    case (11):
+                        Console.WriteLine("a estaçao é primavera");
+                        break;
+                    case (12):
+                        Console.WriteLine("me informe o dia:");
+                        dia = int.Parse(Console.ReadLine());
+                        if (dia >= 21)
+                            Console.WriteLine("a estaçao é verão");
+                        else
+                            Console.WriteLine("A estação é primavera");
+                        break;
+                    case 13:
+                        Console.WriteLine("seu programa esta encerrando");
+                        break;
+                    default:
+                        Console.WriteLine("mes invalido");
+                        break;
+                }
+            }while(mes!= 13);
         }
         public static void Exercicio6()
         {
